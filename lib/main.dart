f13d438b5f84a4ae7bb006241183241fdb0bdd0c
leonardo-johnc <leojohndoe94gmail.com>
2024-12-12 19:17:53 -0700

    did rollback on changes made since it gave me 99 problems and it was all my fault

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:rdp_todolist/screens/home_page.dart';
 import 'firebase_options.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
 //initialize flutter bondings
   WidgetsFlutterBinding.ensureInitialized();
 
+
   //intialize flutter with the current platform's default options
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  /*load environment variables from the .env file 
-  since i'm getting a warning of leak on github and told me to hide api key in a .env file and place it in .gitignore*/
-   await dotenv.load();
 
   runApp(const MainApp());
 }
